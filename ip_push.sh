dadee6ceb34521a2fab1a8eb8831d9ed9ad8da7e
Jetson <vyamamot@uci.edu>
2022-02-22 23:15:45 -0800

    new script

--- ip_push.sh
+++ ip_push.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-IP_FILE=/home/$(whoami)/VNC_IP/testfile.txt
+USER=valen
+ID=valentest
+IP_FILE=/home/$USER/VNC_IP/jetson-$ID.txt
 
 CURRENT_IP=$(ifconfig wlan0 | grep inet | head -n 1 | awk '{print $2}')
 
 echo $CURRENT_IP > $IP_FILE
+cd /home/$USER/VNC_IP/
+runuser -l $USER -c "cd /home/$USER/VNC_IP; git add $IP_FILE"
+runuser -l $USER -c "cd /home/$USER/VNC_IP; git commit -m'ip change'"
+runuser -l $USER -c "cd /home/$USER/VNC_IP; git push origin master"
