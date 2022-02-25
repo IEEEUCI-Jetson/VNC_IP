35fd67bcf3049e03c2049c4d83385e8b4f509f69
Jetson <vyamamot@uci.edu>
2022-02-25 01:10:45 -0800

    ip push update

--- ip_push.sh
+++ ip_push.sh
@@ -4,10 +4,15 @@ USER=valen
 ID=valentest
 IP_FILE=/home/$USER/VNC_IP/jetson-$ID.txt
 
-CURRENT_IP=$(ifconfig wlan0 | grep inet | head -n 1 | awk '{print $2}')
+#CURRENT_IP=$(ifconfig wlan0 | grep inet | head -n 1 | awk '{print $2}')
+CURRENT_IP=$(hostname -I | awk '{print $1}')
+
+echo "HELLO\n" >> /home/valen/out
 
 echo $CURRENT_IP > $IP_FILE
 cd /home/$USER/VNC_IP/
-runuser -l $USER -c "cd /home/$USER/VNC_IP; git add $IP_FILE"
-runuser -l $USER -c "cd /home/$USER/VNC_IP; git commit -m'ip change'"
-runuser -l $USER -c "cd /home/$USER/VNC_IP; git push origin master"
+git add $IP_FILE
+git commit -m'ip change'
+git push origin master
+echo $? >> /home/valen/out
+echo $CURRENT_IP >> /home/valen/out
